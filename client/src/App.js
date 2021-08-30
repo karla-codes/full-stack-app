623edfe776dd173bc23750fb1da1d3682c8be64a
karla-walker <indiablonix@gmail.com>
2021-08-29 21:39:41 -0700

    Testing render of Courses component

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,32 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+// import { Router, Route, Switch } from 'react-router';
+import Courses from './components/Courses';
 
+// main container Component
 function App() {
-  const [data, setData] = useState('');
+  // set up routes
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/courses')
-      .then(res => res.json())
-      .then(resData => {
-        setData(resData.courses);
-        console.log(data);
-      })
-      .catch(err => console.log('Error fetching data', err));
-  }, []);
-
-  return (
-    <div className="App">
-      <ul>
-        {data.map(course => (
-          <li>{course.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <Courses />;
+  // const [data, setData] = useState('');
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/api/courses')
+  //     .then(res => res.json())
+  //     .then(resData => {
+  //       setData(resData.courses);
+  //       console.log(data);
+  //     })
+  //     .catch(err => console.log('Error fetching data', err));
+  // }, [data]);
+  // return (
+  //   <div className="App">
+  //     <ul>
+  //       {data.map(course => (
+  //         <li>{course.title}</li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // );
 }
 
 export default App;
