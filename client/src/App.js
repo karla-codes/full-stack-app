4158ca85376b41bf5662be9ec226fe3c24c9059e
karla-walker <indiablonix@gmail.com>
2021-09-01 00:34:49 -0700

    Imported BrowserRouter and added initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,40 @@
-import './App.css';
+import './styles/global.css';
 import React from 'react';
-// import { Router, Route, Switch } from 'react-router';
-// import Courses from './components/Courses';
-import CourseDetail from './components/CourseDetail';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// components
+import Header from './components/Header';
+import Courses from './components/Courses';
+// import CourseDetail from './components/CourseDetail';
+// import CreateCourse from './components/CreateCourse';
+// import UpdateCourse from './components/UpdateCourse';
+// import UserSignIn from './components/UserSignIn';
+import UserSignUp from './components/UserSignUp';
+// import UserSignOut from './components/UserSignOut';
+import withContext from './Context/Context';
+
+// components w/Context
+const CoursesWithContext = withContext(Courses);
+const UserSignUpWithContext = withContext(UserSignUp);
 
 // main container Component
 function App() {
   // set up routes
+  return (
+    <Router>
+      <Header />
 
-  return <CourseDetail />;
-  // const [data, setData] = useState('');
-  // useEffect(() => {
-  //   fetch('http://localhost:5000/api/courses')
-  //     .then(res => res.json())
-  //     .then(resData => {
-  //       setData(resData.courses);
-  //       console.log(data);
-  //     })
-  //     .catch(err => console.log('Error fetching data', err));
-  // }, [data]);
-  // return (
-  //   <div className="App">
-  //     <ul>
-  //       {data.map(course => (
-  //         <li>{course.title}</li>
-  //       ))}
-  //     </ul>
-  //   </div>
-  // );
+      <Switch>
+        <Route exact path="/" component={CoursesWithContext} />
+        {/* <Route path="/courses/create" component={CreateCourse} /> */}
+        {/* <Route path="/courses/:id/update" component={UpdateCourse} /> */}
+        {/* <Route path="/courses/:id" component={CourseDetail} /> */}
+        {/* <Route path="/signin" component={UserSignIn} /> */}
+        <Route path="/signup" component={UserSignUpWithContext} />
+        {/* <Route path="/signout" component={UserSignOut} /> */}
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
