d47ba50e24ba3c67ddf901ed5925f2bff23c4c31
karla-walker <indiablonix@gmail.com>
2021-09-01 19:18:45 -0700

    Added DisplayErrors function component to Form

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Form(props) {
   // const { actions } = this.props.context;
-  const { submit } = props;
+  const { submit, submitButtonText, cancel, errors } = props;
   const { elements } = props;
 
   function handleSubmit(e) {
@@ -10,19 +10,46 @@ export default function Form(props) {
     submit();
   }
 
+  function handleCancel(e) {
+    e.preventDefault();
+    cancel();
+  }
+
   return (
     <main>
+      <DisplayErrors errors={errors} />
       <div className="form--centered">
         <form onSubmit={handleSubmit}>
           {elements()}
           <button className="button" type="submit">
-            Sign Up
+            {submitButtonText}
           </button>
-          <button className="button button-secondary" onClick="">
+          <button className="button button-secondary" onClick={handleCancel}>
             Cancel
           </button>
         </form>
       </div>
     </main>
   );
+}
+
+function DisplayErrors({ errors }) {
+  let errorsDisplay = null;
+
+  if (errors.length) {
+    errorsDisplay = (
+      <div>
+        <h2>Validation Errors</h2>
+        <div className="validation--errors">
+          <ul>
+            {errors.map(error => (
+              <li>{error}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+  return errorsDisplay;
 }
