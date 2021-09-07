06b48000b2970d7876e57b506fdac3697b8ad441
karla-walker <indiablonix@gmail.com>
2021-09-06 20:24:20 -0700

    Clean up

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -54,35 +54,6 @@ function UserSignUp(props) {
         </React.Fragment>
       )}
     />
-    /* <main>
-      <div className="form--centered">
-        <h2>Sign Up</h2>
-        <form>
-          <label htmlFor="firstName">First Name</label>
-          <input id="firstName" name="firstName" type="text" value=""></input>
-          <label htmlFor="lastName">Last Name</label>
-          <input id="lastName" name="lastName" type="text" value=""></input>
-          <label htmlFor="emailAddress">Email Address</label>
-          <input
-            id="emailAddress"
-            name="emailAddress"
-            type="email"
-            value=""
-          ></input>
-          <label htmlFor="password">Password</label>
-          <input id="password" name="password" type="password" value=""></input>
-          <button className="button" type="submit">
-            Sign Up
-          </button>
-          <button className="button button-secondary" onClick="">
-            Cancel
-          </button>
-        </form>
-        <p>
-          Already have a user account? Click here to <Link to="/signin"></Link>
-        </p>
-      </div>
-    </main> */
   );
 
   function change(e) {
