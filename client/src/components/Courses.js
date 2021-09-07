edc040d6786f532a9f7feab89a514f97bb921592
karla-walker <indiablonix@gmail.com>
2021-09-07 02:50:08 -0700

    Added isUserAuthenticated function to check if user is auth before creating a new course

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -6,6 +6,8 @@ function Courses(props) {
   const [courses, setCourses] = useState([]);
   const { context } = props;
 
+  const authUser = context.authenticatedUser;
+
   useEffect(() => {
     context.data
       .getCourses()
@@ -18,6 +20,7 @@ function Courses(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // for each course, return anchor tag with course info
   const coursesArr = courses.map(course => {
     return (
       <Link
@@ -31,37 +34,54 @@ function Courses(props) {
     );
   });
 
+  function isUserAuthenticated() {
+    if (authUser) {
+      return (
+        <Link
+          to="/courses/create"
+          className="course--module course--add--module"
+        >
+          <span className="course--add--title">
+            <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="0 0 13 13"
+              className="add"
+            >
+              <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+            </svg>
+            New Course
+          </span>
+        </Link>
+      );
+    } else {
+      return (
+        <Link to="/signin" className="course--module course--add--module">
+          <span className="course--add--title">
+            <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="0 0 13 13"
+              className="add"
+            >
+              <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+            </svg>
+            New Course
+          </span>
+        </Link>
+      );
+    }
+  }
+
   return (
     <div className="wrap main--grid">
-      {/* for each course, return anchor tag with course info */}
-
-      {/* {courses.map(course => (
-        <Link
-          to={`/courses/${course.id}`}
-          className="course--module course--link"
-        >
-          <h2 className="course--label">{course.title}</h2>
-          <h3 className="course--title">{course.description}</h3>
-        </Link>
-      ))} */}
       {coursesArr}
-
       {/* Link to add new course */}
-      <Link to="/courses/create" className="course--module course--add--module">
-        <span className="course--add--title">
-          <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            viewBox="0 0 13 13"
-            className="add"
-          >
-            <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-          </svg>
-          New Course
-        </span>
-      </Link>
+      {isUserAuthenticated()}
     </div>
   );
 }
