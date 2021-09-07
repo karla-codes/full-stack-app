1239df35cc1f3d4c40efad33bcf1ba3d544e752e
karla-walker <indiablonix@gmail.com>
2021-09-06 19:20:48 -0700

    Imported CourseDetail and Forbidden components and rearranged routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,20 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // components
 import Header from './components/Header';
 import Courses from './components/Courses';
-// import CourseDetail from './components/CourseDetail';
+import CourseDetail from './components/CourseDetail';
 import CreateCourse from './components/CreateCourse';
 import UpdateCourse from './components/UpdateCourse';
-// import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
 import UserSignIn from './components/UserSignIn';
-// import UserSignOut from './components/UserSignOut';
 import withContext from './Context/Context';
 import PrivateRoute from './PrivateRoute';
 import UserSignOut from './components/UserSignOut';
+import Forbidden from './components/Forbidden';
 
 // components w/Context
 const HeaderWithContext = withContext(Header);
 const CoursesWithContext = withContext(Courses);
+const CourseDetailWithContext = withContext(CourseDetail);
 const UserSignUpWithContext = withContext(UserSignUp);
 const UserSignInWithContext = withContext(UserSignIn);
 const UserSignOutWithContext = withContext(UserSignOut);
@@ -35,17 +35,18 @@ function App() {
       <Switch>
         <Route exact path="/" component={CoursesWithContext} />
         <PrivateRoute
-          path="/courses/create"
-          component={CreateCourseWithContext}
-        />
-        <PrivateRoute
           path="/courses/:id/update"
           component={UpdateCourseWithContext}
         />
-        {/* <Route path="/courses/:id" component={CourseDetail} /> */}
+        <PrivateRoute
+          path="/courses/create"
+          component={CreateCourseWithContext}
+        />
+        <Route path="/courses/:id" component={CourseDetailWithContext} />
         <Route path="/signin" component={UserSignInWithContext} />
         <Route path="/signup" component={UserSignUpWithContext} />
         <Route path="/signout" component={UserSignOutWithContext} />
+        <Route path="/forbidden" component={Forbidden} />
       </Switch>
     </Router>
   );
