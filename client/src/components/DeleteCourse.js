22e474403a5a6cdc47acfd597f910ce0659a0039
karla-walker <indiablonix@gmail.com>
2021-09-07 23:17:41 -0700

    Created DeleteCourse component

---
+++ client/src/components/DeleteCourse.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function DeleteCourse(props) {
+  const { context } = props;
+  const { id } = useParams();
+  const authUser = context.authenticatedUser;
+  return (
+    <main>
+      <div className="wrap">
+        <h2>Delete</h2>
+        <p>Are you sure you want to delete this course?</p>
+        <form onSubmit={deleteCourse}>
+          <button className="button" type="submit">
+            Delete
+          </button>
+          <button className="button button-secondary" onClick={cancel}>
+            Cancel
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+
+  function deleteCourse(e) {
+    e.preventDefault();
+    context.data
+      .deleteCourse(id, authUser)
+      .then(() => {
+        props.history.push('/');
+      })
+      .catch(err => console.log(err));
+  }
+
+  function cancel() {
+    props.history.push('/');
+  }
+}
+
+export default DeleteCourse;
